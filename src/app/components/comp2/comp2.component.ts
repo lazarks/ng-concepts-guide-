640538a195736e54d7d3954b3048aa86c13303e1
lazarks <sljivic.lazar037@gmail.com>
2022-01-31 19:28:53 +0100

    rxjs operators

--- src/app/components/comp2/comp2.component.ts
+++ src/app/components/comp2/comp2.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { debounceTime, from, interval, Observable } from 'rxjs';
+import {
+  count,
+  debounceTime,
+  distinct,
+  elementAt,
+  filter,
+  first,
+  from,
+  interval,
+  last,
+  max,
+  min,
+  Observable,
+  of,
+  skip,
+  take,
+  takeLast,
+  takeWhile,
+} from 'rxjs';
 
 @Component({
   selector: 'app-comp2',
@@ -9,6 +27,22 @@ import { debounceTime, from, interval, Observable } from 'rxjs';
 })
 export class Comp2Component implements OnInit {
   searchForm!: FormGroup;
+
+  categories = [
+    'TV',
+    'Mobiles',
+    'Chargers',
+    'Some',
+    'Highlight',
+    'TV',
+    'Chargers',
+    'TV',
+    'Headphones',
+  ];
+  category$: Observable<string> = from(this.categories);
+
+  ranks = [1, 56, 45, 2, 7, 33, 76, 2, 23, 43, 26, 56];
+  rank$: Observable<any> = from(this.ranks);
 
   constructor(private fb: FormBuilder) {}
 
@@ -19,9 +53,46 @@ export class Comp2Component implements OnInit {
 
     this.searchForm
       .get('name')
-      ?.valueChanges.pipe(debounceTime(500))
-      .subscribe((data) => console.log(data));
+      ?.valueChanges.pipe(
+        // take(5),
+        // takeWhile((v) => this.checkCondition(v)),
+        filter((v) => this.checkCharCount(v)),
+        debounceTime(500)
+      )
+      .subscribe((data) => {
+        console.log(data);
+
+        this.rank$
+          .pipe(
+            distinct(),
+            filter((v) => v <= 35),
+            // max()
+            min()
+          )
+          .subscribe((data) => console.log(data));
+
+        // this.category$.pipe(skip(2), distinct(), count()).subscribe((data) => {
+        //   console.log(data);
+        // });
+
+        // this.category$
+        //   .pipe(
+        //     // takeLast(2),
+        //     // first(),
+        //     // last(),
+        //     elementAt(2)
+        //   )
+        //   .subscribe((data2) => {
+        //     console.log(data2);
+        //   });
+      });
   }
 
-  readValue() {}
+  checkCharCount(value: string) {
+    return value.length <= 10;
+  }
+
+  checkCondition(value: string) {
+    return value.length <= 5;
+  }
 }
