f94d6e405244bfc3440ef7fd41a628ec41552abe
lazarks <sljivic.lazar037@gmail.com>
2022-02-18 03:09:30 +0100

    switched navbar to sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'rxjs';
+  showNav: boolean = false;
+
+  toggleNav() {
+    this.showNav = !this.showNav;
+  }
 }
