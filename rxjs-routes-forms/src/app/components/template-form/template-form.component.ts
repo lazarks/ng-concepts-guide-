2f84081ee29f896570e50b355c97f2efdfb75e6b
lazarks <sljivic.lazar037@gmail.com>
2022-02-23 15:13:19 +0100

    simple form example

--- rxjs-routes-forms/src/app/components/template-form/template-form.component.ts
+++ rxjs-routes-forms/src/app/components/template-form/template-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,42 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./template-form.component.scss'],
 })
 export class TemplateFormComponent implements OnInit {
+  @ViewChild('f') signUpForm!: NgForm;
+  submitted = false;
+  user = {
+    username: '',
+    email: '',
+    gender: '',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  onFormSubmit(form: NgForm) {
-    console.log('submited');
-    console.log(form);
+  @HostListener('document:keyup', ['$event']) hanelKeyboardEvent(
+    event: KeyboardEvent
+  ) {
+    if (event.key == '+') {
+      this.fillValues();
+    }
+  }
+
+  fillValues() {
+    this.signUpForm.form.patchValue({
+      loginData: {
+        email: 'lazar@gmail.com',
+        username: 'lzr123',
+      },
+      // gender: "male" // setValue must include all cntls
+    });
+  }
+
+  onFormSubmit() {
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.loginData.username;
+    this.user.email = this.signUpForm.value.loginData.email;
+    this.user.gender = this.signUpForm.value.gender;
+    console.log(this.user);
+    this.signUpForm.reset();
   }
 }
