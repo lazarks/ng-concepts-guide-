13ef71da308c0df125c8a9d5a20de6a585ac6393
lazarks <sljivic.lazar037@gmail.com>
2022-02-23 16:52:48 +0100

    created user data nad template

--- rxjs-routes-forms/src/app/components/filter-pipes/filter-pipes.component.ts
+++ rxjs-routes-forms/src/app/components/filter-pipes/filter-pipes.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-filter-pipes',
   templateUrl: './filter-pipes.component.html',
-  styleUrls: ['./filter-pipes.component.scss']
+  styleUrls: ['./filter-pipes.component.scss'],
 })
 export class FilterPipesComponent implements OnInit {
+  users = [
+    {
+      name: 'Name1',
+      joinDate: new Date(15, 2, 2022),
+    },
+    {
+      name: 'Name2',
+      joinDate: new Date(20, 2, 2022),
+    },
+    {
+      name: 'Name3',
+      joinDate: new Date(25, 2, 2022),
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
