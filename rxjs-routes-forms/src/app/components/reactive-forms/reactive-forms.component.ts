192a6c140c0e2628d61c0895cc48bf1f6524a7cb
lazarks <sljivic.lazar037@gmail.com>
2022-02-23 16:27:55 +0100

    custom validators

--- rxjs-routes-forms/src/app/components/reactive-forms/reactive-forms.component.ts
+++ rxjs-routes-forms/src/app/components/reactive-forms/reactive-forms.component.ts
@@ -8,18 +8,18 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveFormsComponent implements OnInit {
   genders = ['male', 'female'];
+  restrictedNames = ['Lazar'];
   signUpForm!: FormGroup;
 
   constructor() {}
 
-  get hobbyControls() {
-    return (<FormArray>this.signUpForm.get('hobbies')).controls;
-  }
-
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.isRestrictedNames.bind(this),
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
 
@@ -32,8 +32,20 @@ export class ReactiveFormsComponent implements OnInit {
     console.log(this.signUpForm);
   }
 
+  get hobbyControls() {
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
+
   onAddHobby() {
     let control = new FormControl(null, Validators.required);
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  isRestrictedNames(control: FormControl) {
+    if (this.restrictedNames.includes(control.value)) {
+      return { nameIsRestricted: true };
+    }
+
+    return null;
   }
 }
