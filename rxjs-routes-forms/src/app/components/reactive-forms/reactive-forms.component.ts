ec5b8a5665839703240d4b1776d7a8752fcfe0fb
lazarks <sljivic.lazar037@gmail.com>
2022-02-23 16:37:54 +0100

    custom async validator

--- rxjs-routes-forms/src/app/components/reactive-forms/reactive-forms.component.ts
+++ rxjs-routes-forms/src/app/components/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -20,7 +27,11 @@ export class ReactiveFormsComponent implements OnInit {
           Validators.required,
           this.isRestrictedNames.bind(this),
         ]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          [this.isRestrictedEmails]
+        ),
       }),
 
       gender: new FormControl(null),
@@ -41,11 +52,27 @@ export class ReactiveFormsComponent implements OnInit {
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
 
+  // custom validator
   isRestrictedNames(control: FormControl) {
     if (this.restrictedNames.includes(control.value)) {
       return { nameIsRestricted: true };
     }
 
     return null;
+  }
+
+  // custom async validator
+  isRestrictedEmails(control: AbstractControl): Promise<any> | Observable<any> {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsRestricted: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+
+    return promise;
   }
 }
