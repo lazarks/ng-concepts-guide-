190023b46823386df54b3a87e3faf3ae5c7bd560
lazarks <sljivic.lazar037@gmail.com>
2022-03-03 19:13:52 +0100

    auth comp template finished

--- authentication/src/app/components/auth/auth.component.ts
+++ authentication/src/app/components/auth/auth.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  isLoginMode: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.isLoginMode = true;
   }
 
+  ngOnInit(): void {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
